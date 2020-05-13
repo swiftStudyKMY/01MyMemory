a3199177adfddb6d1d53fe883123d2d883402971
kmy_macmini <801sanae@gmail.com>
2020-05-14 00:46:19 +0900

    init 및 오토레이아웃 일단 패스

---
+++ 01MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  01MyMemory
+//
+//  Created by 김민영 on 14/05/2020.
+//  Copyright © 2020 김민영. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ 01MyMemory/MemoData.swift
@@ -0,0 +1,23 @@
+//
+//  MemoData.swift
+//  01MyMemory
+//
+//  Created by 김민영 on 14/05/2020.
+//  Copyright © 2020 김민영. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemoData {
+    var memoInx : Int?
+    
+    var title : String?
+    
+    var contents : String?
+    
+    var image : UIImage?
+    
+    var regdate : Date?
+    
+}

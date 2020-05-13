d5cc0cc68d4f1ad1a96895185c7aa3e79b02ce7c
kmy_macmini <801sanae@gmail.com>
2020-05-13 23:39:21 +0900

    Initial Commit

---
+++ 01MyMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01MyMemory
+//
+//  Created by 김민영 on 13/05/2020.
+//  Copyright © 2020 김민영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
